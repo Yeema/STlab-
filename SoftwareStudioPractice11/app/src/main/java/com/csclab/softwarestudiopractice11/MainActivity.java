ed0e1475eacc4404afe17b68f512f1eebe413555
Yeema <e2219755@gmail.com>
2016-05-26 18:59:04 +0800

    hi

---
+++ SoftwareStudioPractice11/app/src/main/java/com/csclab/softwarestudiopractice11/MainActivity.java
@@ -0,0 +1,53 @@
+package com.csclab.softwarestudiopractice11;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+
+public class MainActivity extends Activity {
+
+    private Button send;
+    private EditText Name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /** To Do:
+         * (1) Find the UI element on xml file
+         * **/
+        Name= (EditText) findViewById(R.id.editText);
+        send= (Button) findViewById(R.id.button);
+
+        send.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                sendName();
+            }
+        });
+
+    }
+
+    public void sendName(){
+
+        /** To Do:
+         * (1) Use intent and Bundle to pass string
+         * (2) Change activity to Activity2
+         * **/
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this, Activity2.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("name", Name.getText().toString());
+        intent.putExtras(bundle);
+        startActivity(intent);
+
+    }
+
+
+
+}
